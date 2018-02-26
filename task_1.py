b208e134ce2d3f4642d1eb4a00c1d96e5d60d7c8
Sandra <sandraskarshaug@gmail.com>
2018-02-26 22:04:09 +0100

    Create list with all headers, split the tweets at an early stage, and change to use sample as the project description says so

--- task_1.py
+++ task_1.py
@@ -1,29 +1,46 @@
-from utils import get_rdd, get_conf, get_context
+from utils import get_tweets, get_conf, get_context
 
 conf = get_conf('Task_1')
 sc = get_context(conf)
+tweets = get_tweets(sc)
+header = ['utc_time', 'country_name', 'country_code', 'place_type', \
+            'place_name', 'language', 'username', 'user_screen_name', \
+            'timezone_offset', 'number_of_friends', 'tweet_text', \
+            'latitude', 'longitude']
 
-#a)
-rdd = get_rdd(sc) #Number of tweets: 2715066
-#print(rdd.count()) 
 
-#b)
-user_rdd = rdd.map(lambda x: x.split('\t')[7]).distinct() #Number of distinct users: 583299
+#1.a)
+print('Number of tweets:' + str(tweets.count())) #Number of tweets when not sampled: 2715066
 
-#print(user_rdd.count())
+#1.b)
+distinct_users = tweets.map(lambda x: x[header.index('username')]).distinct() #Number of distinct users when not sampled: 583299
+print('Number of distinct users: ' + str(distinct_users))
 
-#c)
-country_rdd = rdd.map(lambda x: x.split('\t')[1]).distinct() #Number of distinct countries: 70
-#print(country_rdd.count())
+#1.c)
+distinct_countries = tweets.map(lambda x: x[header.index('country_name')]).distinct() #Number of distinct countries when not sampled: 70
+print('Number of distinct countries: ' + str(distinct_countries.count()))
 
-#d)
-place_name_rdd = rdd.map(lambda x: x.split('\t')[4]).distinct() #Number of distinct place names: 23121
-#print(place_name_rdd.count())
+#1.d)
+distinct_placenames = tweets.map(lambda x: x[header.index('place_name')]).distinct() #Number of distinct place names when not sampled: 23121
+print('Number of distinct place names: ' + str(distinct_placenames.count()))
 
-#e)
-language_rdd = rdd.map(lambda x: x.split('\t')[5]).distinct() #Number of distinct languages: 46
-#print(language_rdd.count())
+#1.e)
+distinct_languages = tweets.map(lambda x: x[header.index('language')]).distinct() #Number of distinct languages when not sampled: 46
+print('Number of distinct languages used in tweets: ' + str(distinct_languages.count()))
 
-#f)
-min_latitude_rdd = rdd.map(lambda x: x.split('\t')[12]).min()
-print(min_latitude_rdd)
+#1.f)
+min_latitude = tweets.map(lambda x: x[header.index('latitude')]).min()
+print('The minimum latitude is: ' + str(min_latitude))
+
+#1.g)
+max_latitude = tweets.map(lambda x: x[header.index('latitude')]).max()
+print('The maxmimum latitude is: ' + str(max_latitude))
+
+#1.h)
+min_longitude = tweets.map(lambda x: x[header.index('longitude')]).min()
+print('The minimum longitude is: ' + str(min_longitude))
+
+#1.i)
+max_longitude =  tweets.map(lambda x: x[header.index('longitude')]).max()
+print('The maximum longitude is: ' + str(max_longitude))
+
--- utils.py
+++ utils.py
@@ -7,5 +7,5 @@ def get_conf(task_name):
 def get_context(conf):
     return SparkContext(conf=conf)
 
-def get_rdd(context):
-    return context.textFile('data/geotweets.tsv')
+def get_tweets(context):
+    return context.textFile('data/geotweets.tsv').map(lambda x: x.split('\t')).sample(False, 0.1, 5)
