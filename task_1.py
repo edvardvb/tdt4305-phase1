5957fc1b5e62dd0591f7c7d6e423cd80b1437146
Sandra <sandraskarshaug@gmail.com>
2018-02-26 15:25:38 +0100

    Find number of distinct countries

--- task_1.py
+++ task_1.py
@@ -4,9 +4,13 @@ conf = get_conf('Task_1')
 sc = get_context(conf)
 
 #a)
-rdd = get_rdd(sc)
-print(rdd.count()) #Number of tweets: 2715066
+rdd = get_rdd(sc) #Number of tweets: 2715066
+#print(rdd.count()) 
 
 #b)
-new_rdd = rdd.map(lambda x: x.split('\t')[7]).distinct() #Number of distinct users: 583299
-print(new_rdd.count())
+user_rdd = rdd.map(lambda x: x.split('\t')[7]).distinct() #Number of distinct users: 583299
+#print(user_rdd.count())
+
+#c)
+country_rdd = rdd.map(lambda x: x.split('\t')[1]).distinct() #Number of distinct countries: 70
+print(country_rdd.count())
