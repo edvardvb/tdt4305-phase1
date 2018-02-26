5f2882b090b6dda7257f03b73eb2c38df2337507
Sandra <sandraskarshaug@gmail.com>
2018-02-26 15:23:21 +0100

    Find number of distinct users

--- task_1.py
+++ task_1.py
@@ -2,6 +2,11 @@ from utils import get_rdd, get_conf, get_context
 
 conf = get_conf('Task_1')
 sc = get_context(conf)
-rdd = get_rdd(sc)
 
-print(rdd.sample(False, 0.1, 5).collect())
+#a)
+rdd = get_rdd(sc)
+print(rdd.count()) #Number of tweets: 2715066
+
+#b)
+new_rdd = rdd.map(lambda x: x.split('\t')[7]).distinct() #Number of distinct users: 583299
+print(new_rdd.count())
