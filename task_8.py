7b6813b8357a9b13aaecb7eccad3a10d5c2333f3
Sandra <sandraskarshaug@gmail.com>
2018-03-06 23:06:59 +0100

    Fix error in column order

--- task_8.py
+++ task_8.py
@@ -4,7 +4,7 @@ from pyspark.sql.types import FloatType
 
 from utils import setup
 
-conf, sc, tweets, result_path = setup(8, sample=False)
+conf, sc, tweets, result_path = setup(81, sample=False)
 spark = SparkSession.builder.master("").enableHiveSupport().getOrCreate()
 tweetdf = spark.createDataFrame(tweets)
 new_tweetdf = tweetdf \
@@ -14,15 +14,13 @@ new_tweetdf = tweetdf \
     .withColumn('place_type', tweetdf._4) \
     .withColumn('place_name', tweetdf._5) \
     .withColumn('language', tweetdf._6) \
-    .withColumn('username', tweetdf._7) \
-    .withColumn('user_screen_name', tweetdf._8) \
+    .withColumn('user_screen_name', tweetdf._7) \
+    .withColumn('username', tweetdf._8) \
     .withColumn('timezone_offset', tweetdf._9) \
     .withColumn('number_of_friends', tweetdf._10) \
     .withColumn('tweet_text', tweetdf._11) \
     .withColumn('latitude', tweetdf._12.cast(FloatType())) \
     .withColumn('longitude', tweetdf._13.cast(FloatType())) \
-
-#new_tweetdf.createTempView("tweets")
 
 result_file = open(result_path, 'w')
 
@@ -34,25 +32,17 @@ result_file.write(str(number_of_tweets)+'\n')
 number_of_users = new_tweetdf.select('username').distinct().count()
 result_file.write(str(number_of_users)+'\n')
 
-#spark.sql("SELECT DISTINCT COUNT(username) FROM tweets").show()
-
 #8.c)
 number_of_countries = new_tweetdf.select('country_name').distinct().count()
 result_file.write(str(number_of_countries)+'\n')
-
-#spark.sql("SELECT DISTINCT COUNT(country_name) FROM tweets").show()
 
 #8.d)
 number_of_places = new_tweetdf.select('place_name').distinct().count()
 result_file.write(str(number_of_places)+'\n')
 
-#spark.sql("SELECT DISTINCT COUNT(place_name) FROM tweets").show()
-
 #8.e)
 number_of_languages = new_tweetdf.select('language').distinct().count()
 result_file.write(str(number_of_languages)+'\n')
-
-#spark.sql("SELECT DISTINCT COUNT(language) FROM tweets").show()
 
 #8.f)
 min_latitude = new_tweetdf.agg({'latitude': 'min'}).collect()[0]
